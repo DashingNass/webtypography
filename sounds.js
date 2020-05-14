414ba078f4719e658cbc51fa02dc1829e361a401
Nasséra <45563624+DashingNass@users.noreply.github.com>
2020-05-14 14:06:38 +0200

    Add files via upload

---
+++ sounds.js
@@ -0,0 +1,17 @@
+// Hier kan je tijden toevoegen in secondes
+// Op die momenten komt er een class op de body.
+
+// voeg cijfertje 37 toe voor aantal seconde bijvoorbeeld
+sounds = [
+    0,
+    9,
+    12,
+    15,
+    16.5,
+    24,
+    25.8,
+    31,
+    32,
+    34,
+    86
+];
